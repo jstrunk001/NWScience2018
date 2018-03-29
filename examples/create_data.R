920aee785f8dc0b42ddc642c9c2e9e36e06cbba7
jacobstrunk001 <12001003134353@FEDIDCARD.GOV>
2018-03-28 18:02:55 -0700

    updates

--- examples/create_data.R
+++ examples/create_data.R
@@ -4,8 +4,8 @@ data_path=file.path(getwd(),"data")
 if(!dir.exists(data_path)) dir.create(data_path)
 
 #create data
-dat=data.frame(x=1:50,y=50:1)
+example=data.frame(x=1:50,y=50:1)
 
 #write data to package data folder
-dat_path=file.path(data_path,"example.csv")
-write.csv(dat,dat_path)
+dat_path=file.path(data_path,"example.rda")
+save(example,file=dat_path)
